37dc0c1ef71826eb6ff31d59171338c0104615f9
lucasmdc <lucasmdclimao@gmail.com>
2021-03-05 21:05:07 -0300

    first commit

--- src/utils/tests.js
+++
@@ -1,5 +0,0 @@
-export function testValueIs (value, type) {
-  const result = Object.prototype.toString.call(value).toLowerCase()
-
-  return result.substring(1, result.length - 1).split(' ')[1] === type
-}
